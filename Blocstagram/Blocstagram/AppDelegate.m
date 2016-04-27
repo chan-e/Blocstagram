0ec9ae60fc206544619e0a8b8c40d5cfa53a1073
Eddy Chan <echan162@gmail.com>
2016-04-26 20:07:08 -0700

    Checkpoint 27 blocstagram rebuilding instagram: completed

--- Blocstagram/Blocstagram/AppDelegate.m
+++ Blocstagram/Blocstagram/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
+#import "ImagesTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -22,7 +22,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = [[ViewController alloc] init];
+    self.window.rootViewController = [[UINavigationController alloc]
+                                      initWithRootViewController:[[ImagesTableViewController alloc] init]];
     
     [self.window makeKeyAndVisible];
     
